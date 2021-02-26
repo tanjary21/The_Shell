242ebcd0788103694c974d35fdbffb27ea9c09b7
tanjary21 <79201814+tanjary21@users.noreply.github.com>
2021-02-26 09:07:02 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,21 @@
 echo "Hello World"
+
+# Shell commands for excercise 1
+
+# Shell commands for excercise 2
+
+# Shell commands for excercise 3
+
+# Shell commands for excercise 4
+
+# Shell commands for excercise 5
+
+# Shell commands for excercise 6
+
+# Shell commands for excercise 7
+
+# Shell commands for excercise 8
+
+# Shell commands for excercise 9
+
+# Shell commands for excercise 10
